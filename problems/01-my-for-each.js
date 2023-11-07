a0d59ec4ae5ac67cafa293a88bb8feef88737aa0
Chase Kliment <stepintoliquidd@gmail.com>
2023-11-06 18:06:21 -0700

    problems 7 and 8 solved

--- problems/01-my-for-each.js
+++ problems/01-my-for-each.js
@@ -29,6 +29,18 @@ function myForEach(array, cb) {
     }
 }
 
+// myForEach(['a', 'b', 'c'], function (el, i) {
+//     console.log(el + ' is at index ' + i);
+// }); // prints
+// // a is at index 0
+// // b is at index 1
+// // c is at index 2
+
+// let test = [];
+// myForEach(['laika', 'belka'], function (el) {
+//     test.push(el.toUpperCase());
+// });
+// console.log(test); // ['LAIKA', 'BELKA']
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myForEach;
